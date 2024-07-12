b4e9fb96a9007deb80a9ed22b1722fc31d479e9f
Ashraful Haque Chowdhury <ashraful.rahi@gmail.com>
2024-07-13 01:29:30 +0600

    chore: network module

---
+++ lib/data/api/api_config.dart
@@ -0,0 +1,10 @@
+// import 'package:injectable/injectable.dart';
+
+// @lazySingleton
+// class ApiConfig {
+//   final String baseUrl;
+
+//   ApiConfig(ApplicationConfig appConfig) : baseUrl = appConfig.apiBaseUrl;
+
+//   String get apiUrl => baseUrl;
+// }
