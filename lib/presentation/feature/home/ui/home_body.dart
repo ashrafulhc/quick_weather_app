dde52e82b89b8151456f7e8d27133f61326661ab
Ashraful Haque Chowdhury <ashraful.rahi@gmail.com>
2024-07-13 02:20:34 +0600

    feat: basic UI for weather

--- lib/presentation/feature/home/ui/home_body.dart
+++ lib/presentation/feature/home/ui/home_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quick_weather_app/presentation/feature/home/ui/weather_widget.dart';
 
 class HomeBody extends StatelessWidget {
   const HomeBody({super.key});
@@ -9,8 +10,28 @@ class HomeBody extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Screen'),
       ),
-      body: const Center(
-        child: Text('Hello Quick Weather App'),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    onChanged: (value) {
+                      print('$value');
+                    },
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.search),
+                ),
+              ],
+            ),
+            const WeatherWidget(),
+          ],
+        ),
       ),
     );
   }
