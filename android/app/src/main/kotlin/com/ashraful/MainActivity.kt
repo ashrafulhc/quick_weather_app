370382a365d6a629110a4ecb1bdfbb0f79a47a38
Ashraful Haque Chowdhury <ashraful.rahi@gmail.com>
2024-07-12 17:42:44 +0600

    chore: initial commit

---
+++ android/app/src/main/kotlin/com/ashraful/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ashraful.quick_weather_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:quick_weather_app/app/app.dart';
+import 'package:quick_weather_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
